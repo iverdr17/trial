7051bdf00af1c44f51528da8acc708450adf9688
Ivan Daniels <iverdr17@gmail.com>
2020-07-23 00:01:33 +0530

    hjhj

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,5 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }love yuuc,sxl,vsdl
+    }love yuuc,sxl,vsdl mxcczm
 }
